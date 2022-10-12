b171e104ddd7b83b69e0445a10e87a5ee0a91093
Phung Nguyen <longphung.nn@gmail.com>
2022-10-13 01:25:59 +1100

    ✨ feat: add layout for progress map

--- src/components/ProgressMap.vue
+++ src/components/ProgressMap.vue
@@ -1,11 +1,29 @@
 <script>
+import VIcon from './VIcon.vue';
+
 export default {
-  name: 'ProgressMap'
+  name: 'ProgressMap',
+  components: {
+    VIcon
+  },
+  data() {
+    return {
+      progress: 0,
+    }
+  },
+  computed: {
+    offset() {
+      return 0
+    }
+  }
 };
 </script>
 
 <template>
-  <div class='progress-map'>progress map</div>
+  <div class='progress-map'>
+    <VIcon name='attendance'/>
+    <span class='student-avatar' :style="`left: ${offset}`"></span>
+  </div>
 </template>
 
 <style scoped>
@@ -13,5 +31,10 @@ export default {
   background: #D9D9D9;
   border-radius: 20px;
   width: 36.5rem;
+  position: relative
+}
+
+.student-avatar {
+  position: absolute;
 }
 </style>
