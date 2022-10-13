a22b7772d7222c26b52cd4d3d49e75c3225e244b
Phung Nguyen <longphung.nn@gmail.com>
2022-10-13 21:43:50 +1100

    💄 style: style progress map

---
+++ src/components/ProgressStudentAvatar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'ProgressStudentAvatar',
+  props: {
+    progress: {
+      required: true,
+      type: Number,
+    },
+  },
+  computed: {
+    // Return percentage of the width of the whole progress bar
+    offset() {
+      return this.progress;
+    }
+  },
+};
+</script>
+
+<template>
+  <span class="student-avatar" :style="`left: ${offset}%`">
+    avatar
+<!--    TODO: Akhmad to fill with student avatar-->
+  </span>
+</template>
+
+<style scoped>
+.student-avatar {
+  position: absolute;
+  top: calc(100% + 0.3125rem);
+}
+</style>
