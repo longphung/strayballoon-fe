dc42420c2d0da685e09dc8edd591cbec1ecf47e0
Phung Nguyen <longphung.nn@gmail.com>
2022-10-16 18:40:25 +1100

    ✨ feat: add icon to top progress and refactor css

--- src/components/TopPerformers.vue
+++ src/components/TopPerformers.vue
@@ -1,12 +1,19 @@
 <script>
+import VIcon from './VIcon.vue';
+
 export default {
-  name: 'TopPerformers'
+  name: 'TopPerformers',
+  components: {
+    VIcon,
+  },
 };
 </script>
 
 <template>
-  <div class='top-performers'><p class="performance-title">Top Performers</p>
-    <div class="perf1">
+  <div class="top-performers">
+    <p class="performance-title">Top Performers</p>
+    <VIcon name="crown" />
+    <div class="perf">
       <div class="icon-perf"></div>
       <div class="perf-text">
         <div class="text1"></div>
@@ -14,7 +21,7 @@ export default {
       </div>
       <div class="perf-score"></div>
     </div>
-    <div class="perf2">
+    <div class="perf">
       <div class="icon-perf"></div>
       <div class="perf-text">
         <div class="text1"></div>
@@ -22,7 +29,7 @@ export default {
       </div>
       <div class="perf-score"></div>
     </div>
-    <div class="perf3">
+    <div class="perf">
       <div class="icon-perf"></div>
       <div class="perf-text">
         <div class="text1"></div>
@@ -35,54 +42,34 @@ export default {
 
 <style scoped>
 .top-performers {
-  background: #D9D9D9;
+  background: #d9d9d9;
   border-radius: 20px;
 }
+
 .performance-title {
   padding-left: 50px;
   font-size: 1.125rem;
   margin-bottom: 4rem;
 }
-.perf1 {
+
+.perf {
   display: flex;
   align-items: center;
   justify-content: space-around;
-  margin: 1rem;
+  margin: 1rem 1rem 0.5rem;
   height: 4rem;
   background: rgba(151, 151, 151, 1);
   border-radius: 10px;
-  margin-bottom: 0.5rem;
   gap: 30px;
 }
-.perf2 {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 1rem;
-  height: 4rem;
-  background: rgba(151, 151, 151, 1);
-  border-radius: 10px;
-  margin-bottom: 0.5rem;
-  gap: 30px;
-}
-.perf3 {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 1rem;
-  height: 4rem;
-  background: rgba(151, 151, 151, 1);
-  border-radius: 10px;
-  margin-bottom: 0.5rem;
-  gap: 30px;
-}
+
 .icon-perf {
   width: 3rem;
   height: 3rem;
   background-color: rgba(217, 217, 217, 1);
   border-radius: 100px;
-
 }
+
 .perf-text {
   width: 8rem;
   height: 3rem;
@@ -92,6 +79,7 @@ export default {
   text-align: center;
   gap: 10px;
 }
+
 .text1 {
   width: 8rem;
   height: 1.5rem;
@@ -99,14 +87,8 @@ export default {
   border-radius: 5px;
   margin-left: 0.5rem;
 }
-.text2 {
-  width: 8rem;
-  height: 1rem;
-  background-color: rgba(217, 217, 217, 1);
-  border-radius: 5px;
-  margin-left: 0.5rem;
-}
-.perf-score{
+
+.perf-score {
   width: 2.7rem;
   height: 2.7rem;
   background-color: rgba(217, 217, 217, 1);
