078ab41d7b663570ecd2bde65f239cabf955a11b
Phung Nguyen <longphung.nn@gmail.com>
2022-10-30 21:41:27 +1100

    🐞 fix: fix bug where env are not loaded properly

--- src/features/index.js
+++ src/features/index.js
@@ -1,4 +1,4 @@
 export const baseUrl = import.meta.env.VITE_BASE_URL;
-const isDev = import.meta.env.VITE_CONNECT_STAGING !== 'false' ? false : import.meta.env.DEV;
+const isDev = import.meta.env.VITE_CONNECT_STAGING === 'true' ? false : import.meta.env.DEV;
 
 export const baseWs = `${isDev ? 'ws' : 'wss'}://${baseUrl}/ws`;
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target:
-          import.meta.env.VITE_CONNECT_STAGING !== 'false'
-            ? 'http://localhost:8000'
-            : 'https://strayballoon.phungnnl.dev',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/ws': {
-        target:
-          import.meta.env.VITE_CONNECT_STAGING !== 'false' ? 'ws://localhost:8000' : 'wss://strayballoon.phungnnl.dev',
-        changeOrigin: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        '/api': {
+          target:
+            process.env.VITE_CONNECT_STAGING === 'true' ? 'https://strayballoon.phungnnl.dev' : 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/ws': {
+          target:
+            process.env.VITE_CONNECT_STAGING === 'true' ? 'wss://strayballoon.phungnnl.dev' : 'ws://localhost:8000',
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
