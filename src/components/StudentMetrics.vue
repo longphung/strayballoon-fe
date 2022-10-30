a7b1e1e369c159a80d3cd1b18bdf7b85a2ce3a6c
Akhmadjon Khamidov <104096281@student.swin.edu.au>
2022-10-31 03:13:02 +1100

    Chart changes

--- src/components/StudentMetrics.vue
+++ src/components/StudentMetrics.vue
@@ -57,51 +57,6 @@ export default {
         ],
       });
     };
-
-    const chart2 = (id) => {
-      const chartEl = document.getElementById(id);
-
-      const qs = [10, 10]; // this should consist of 2x variables for correct questions and total questions let qs = [cor_qs,total_qs]
-      const questionsLabels = ['Correct', 'Incorrect'];
-
-      this.myChart2 = new Chart(chartEl, {
-        type: 'doughnut',
-        data: {
-          labels: questionsLabels,
-          datasets: [
-            {
-              label: 'Questions answered & total correct',
-              data: qs,
-              backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
-            },
-          ],
-        },
-        options,
-        // plugin for inner text visualisation
-        plugins: [
-          {
-            id: 'text',
-            // beforeDraw: (chart, a, b) => {
-            beforeDraw: (chart) => {
-              const { width, height, ctx } = chart;
-
-              ctx.restore();
-              const fontSize = (height / 160).toFixed(2);
-              // how to add weight / bold??
-              ctx.font = `${fontSize}em sans-serif`;
-              ctx.textBaseline = 'middle';
-              // this variable should be (correctquestions+"/"+totalquestions)
-              const text = '10' + '/' + '20';
-              const textX = Math.round((width - ctx.measureText(text).width) / 2);
-              const textY = height / 1.65;
-
-              ctx.fillText(text, textX, textY);
-              ctx.save();
-            },
-          },
-        ],
-      });
-    };
     const chart3 = (id) => {
       const chartEl = document.getElementById(id);
 
@@ -136,51 +91,6 @@ export default {
               ctx.textBaseline = 'middle';
               // this variable should be (correctquestions+"/"+totalquestions)
               const text = '4' + '/' + '20';
-              const textX = Math.round((width - ctx.measureText(text).width) / 2);
-              const textY = height / 1.65;
-
-              ctx.fillText(text, textX, textY);
-              ctx.save();
-            },
-          },
-        ],
-      });
-    };
-
-    const chart4 = (id) => {
-      const chartEl = document.getElementById(id);
-
-      const qs = [160, 200]; // this should consist of 2x variables for correct questions and total questions let qs = [cor_qs,total_qs]
-      const questionsLabels = ['Correct', 'Incorrect'];
-
-      this.myChart4 = new Chart(chartEl, {
-        type: 'doughnut',
-        data: {
-          labels: questionsLabels,
-          datasets: [
-            {
-              label: 'Questions answered & total correct',
-              data: qs,
-              backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
-            },
-          ],
-        },
-        options,
-        // plugin for inner text visualisation
-        plugins: [
-          {
-            id: 'text',
-            // beforeDraw: (chart, a, b) => {
-            beforeDraw: (chart) => {
-              const { width, height, ctx } = chart;
-
-              ctx.restore();
-              const fontSize = (height / 160).toFixed(2);
-              // how to add weight / bold??
-              ctx.font = `${fontSize}em sans-serif`;
-              ctx.textBaseline = 'middle';
-              // this variable should be (correctquestions+"/"+totalquestions)
-              const text = '160' + '/' + '200';
               const textX = Math.round((width - ctx.measureText(text).width) / 2);
               const textY = height / 1.65;
 
@@ -282,9 +192,7 @@ export default {
     };
 
     chart1('myChart');
-    chart2('myChart2');
     chart3('myChart3');
-    chart4('myChart4');
     chart5('myChart5');
     chart6('myChart6');
   },
@@ -305,21 +213,9 @@ export default {
       </div>
     </div>
     <div class="metrics">
-      <div class="metrics-heading">TOTAL NUMBER OF UNIQUE PROBLEMS SOLVED</div>
-      <div class="numbers">
-        <canvas id="myChart2" class="m-auto" width="225" height="225"></canvas>
-      </div>
-    </div>
-    <div class="metrics">
       <div class="metrics-heading">TOTAL NUMBER OF ERRORS</div>
       <div class="numbers">
         <canvas id="myChart3" class="m-auto" width="225" height="225"></canvas>
-      </div>
-    </div>
-    <div class="metrics">
-      <div class="metrics-heading">TOTAL SCORE</div>
-      <div class="numbers">
-        <canvas id="myChart4" class="m-auto" width="225" height="225"></canvas>
       </div>
     </div>
     <div class="metrics">
@@ -339,10 +235,10 @@ export default {
 
 <style scoped>
 .student-metrics {
-  background: #bfdbfe;
   border-radius: 20px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  border: solid #446894 0.5rem;
 }
 
 .metrics {
@@ -363,7 +259,6 @@ export default {
 }
 
 .numbers {
-  background-color: #479ac8;
   border-radius: 0.6rem;
   margin: 0.2rem;
   height: 250px;
